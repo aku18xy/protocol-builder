e0a557138b0fa786b07ac473a7b86cc285489739
nas <nash18xy@gmail.com>
2026-09-23 05:33:24 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-import { viteSingleFile } from 'vite-plugin-singlefile'
-import tailwindcss from '@tailwindcss/vite' // <-- Import the new Tailwind v4 plugin
-
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <-- Add it to the plugins list
-    viteSingleFile() 
-  ]
-=======
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
->>>>>>> 8904274b7fc74ca3907f95b9ff7e2ac60555bbe9
 })
